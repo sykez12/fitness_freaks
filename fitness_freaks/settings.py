2f33a4a8569b6bee03f92e20552fe01e0ca3d0b3
sykez12 <adam_paddy@hotmail.co.uk>
2021-02-26 16:13:13 +0000

    Added class methods to webhook handler and webhook view

--- fitness_freaks/settings.py
+++ fitness_freaks/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = '5cc2b_t8$_lrkfpfh%^@+3)hm_xhb(k-wt!mv$@aboa_3arysc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -167,3 +167,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
