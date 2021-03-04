b7c96d8ef9f46f0b58402eda835ef938db55341e
sykez12 <adam_paddy@hotmail.co.uk>
2021-03-04 15:25:21 +0000

    Added depolyment settings

--- fitness_freaks/settings.py
+++ fitness_freaks/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '5cc2b_t8$_lrkfpfh%^@+3)hm_xhb(k-wt!mv$@aboa_3arysc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fitness-freaks-website.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -114,12 +114,17 @@ WSGI_APPLICATION = 'fitness_freaks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
