1c71360640606b061849d744bcd10faabfeaee19
sykez12 <adam_paddy@hotmail.co.uk>
2021-03-06 14:20:44 +0000

    Removed secret key and set debug

--- fitness_freaks/settings.py
+++ fitness_freaks/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5cc2b_t8$_lrkfpfh%^@+3)hm_xhb(k-wt!mv$@aboa_3arysc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitness-freaks-website.herokuapp.com', 'localhost']
 
