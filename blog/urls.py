62cd53093988e5ba4a0b5e32ac34c0881ae04ada
sykez12 <adam_paddy@hotmail.co.uk>
2021-03-13 04:14:36 +0000

    Implement a comments model

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('post_list/', views.PostList.as_view(), name='post_list'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
